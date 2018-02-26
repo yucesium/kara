15a0bf492fa57c07a31f33e8c67b8210c6961998
yucesium <kyoudan12@gmail.com>
2018-02-27 06:24:59 +0900

    Initial Commit

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,1 +1,2 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
+viewer.dataSources.add(Cesium.KmlDataSource.load('https://yucesium.github.io/kara/cesium-starter-app-master/Source/mizu.kml'));
